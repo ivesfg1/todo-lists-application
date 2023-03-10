590deaccc63887f441f1db8cefbfb19ab2dbb915
ivesfg1 <wigzfurtado@gmail.com>
2023-03-10 19:54:25 -0300

    refatorando com sugestao do ChatGPT

--- backend/apps/todo/admin.py
+++ backend/apps/todo/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from .models import TodoList, TodoTask
+from . import models
 
-admin.site.register(TodoList)
-admin.site.register(TodoTask)
+
+# TODO: Adicionar maneira de filtrar no admin pelas listas e tarefas que
+# ja estao completas. É algo no list_filter, tenho certeza, só nao sei
+# como faz pois nao da pra usar property, mas nao sei se vale a pena
+# fazer um annotate no get_queryset do manager, ficaria estranho pois
+# eu ja tenho um metodo complete() no meu Queryset personalizado...
+# enfim, ver isso depois
+@admin.register(models.TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.TodoTask)
+class TodoTaskAdmin(admin.ModelAdmin):
+    pass
